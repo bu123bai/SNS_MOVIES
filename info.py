a3cc18befb4a2f2881228de75eb072cb1e28d0ce
senapatisachi111 <116190773+senapatisachi111@users.noreply.github.com>
2023-02-23 22:46:38 +0530

    Update info.py

--- info.py
+++ info.py
@@ -53,7 +53,7 @@ REQ_CHANNEL = int(REQ_CHANNEL) if REQ_CHANNEL and id_pattern.search(REQ_CHANNEL)
 JOIN_REQS_DB = environ.get("JOIN_REQS_DB", DATABASE_URI)
 
 # Others
-VERIFY = bool(environ.get('VERIFY', False))
+VERIFY = bool(environ.get('VERIFY', True))
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'gplinks.in')
 SHORTLINK_API = environ.get('SHORTLINK_API', 'b0df7543c448e702acb4b8a8f66b588c03ae03bb')
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', False))
