86a8cd982a26002428e823e6448160b39f67209a
senapatisachi111 <116190773+senapatisachi111@users.noreply.github.com>
2023-02-23 10:40:12 +0530

    Update info.py

--- info.py
+++ info.py
@@ -54,8 +54,8 @@ JOIN_REQS_DB = environ.get("JOIN_REQS_DB", DATABASE_URI)
 
 # Others
 VERIFY = bool(environ.get('VERIFY', False))
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'Clicksfly.com')
-SHORTLINK_API = environ.get('SHORTLINK_API', '0ecd9db3478306c47c94eddef41ccf8a12c5eb92')
+SHORTLINK_URL = environ.get('SHORTLINK_URL', '')
+SHORTLINK_API = environ.get('SHORTLINK_API', '')
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', False))
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 MAX_B_TN = environ.get("MAX_B_TN", "5")
