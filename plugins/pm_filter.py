a5659d62776e6969b42484e8948af38ba80d4d0e
senapatisachi111 <116190773+senapatisachi111@users.noreply.github.com>
2023-03-07 02:34:40 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -61,7 +61,7 @@ async def pm_text(bot, message):
     user_id = message.from_user.id
     if content.startswith("/") or content.startswith("#"): return  # ignore commands and hashtags
     if user_id in ADMINS: return # ignore admins
-    await message.reply_text("Yᴏᴜʀ ᴍᴇssᴀɢᴇ ʜᴀs ʙᴇᴇɴ sᴇɴᴛ ᴛᴏ ᴍʏ ᴍᴏᴅᴇʀᴀᴛᴏʀs !\n\n<b>I Cᴀɴ Pʀᴏᴠɪᴅᴇ Mᴏᴠɪᴇꜱ Oɴʟʏ Iɴ Gʀᴏᴜᴘꜱ.</b> Pʟᴇᴀꜱᴇ Jᴏɪɴ Oᴜʀ Mᴀɪɴ Gʀᴏᴜᴘ ❤️\n\n @MOVIES_GROUP_SNS")
+    await message.reply_text("Yᴏᴜʀ ᴍᴇssᴀɢᴇ ʜᴀs ʙᴇᴇɴ sᴇɴᴛ ᴛᴏ ᴍʏ ᴍᴏᴅᴇʀᴀᴛᴏʀs !\n\n<b>I Cᴀɴ Pʀᴏᴠɪᴅᴇ Mᴏᴠɪᴇꜱ Oɴʟʏ Iɴ Gʀᴏᴜᴘꜱ.</b> Pʟᴇᴀꜱᴇ Jᴏɪɴ Oᴜʀ Mᴀɪɴ Gʀᴏᴜᴘ. Tᴏᴜᴄʜ Oɴ Mʏ Pʀᴏғɪʟᴇ Pɪᴄᴛᴜʀᴇ Tᴏ Gᴇᴛ Oᴜʀ Gʀᴏᴜᴘ Lɪɴᴋ ❤️ ")
     await bot.send_message(
         chat_id=LOG_CHANNEL,
         text=f"<b>#𝐏𝐌_𝐌𝐒𝐆\n\nNᴀᴍᴇ : {user}\n\nID : {user_id}\n\nMᴇssᴀɢᴇ : {content}</b>"
