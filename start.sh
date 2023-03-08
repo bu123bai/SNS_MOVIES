8bf7a5d00f39fec25a2d062ca7a1b79080587726
bu123bai <120026811+bu123bai@users.noreply.github.com>
2023-03-08 13:12:45 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/senapatisachi111/SNS_MOVIES.git /SNS_MOVIES
+  git clone https://github.com/bu123bai/SNS_MOVIES.git /SNS_MOVIES
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /SNS_MOVIES
 fi
 cd /SNS_MOVIES
 pip3 install -U -r requirements.txt
-echo "Starting DQ-The-File-Donor...."
+echo "Starting SNS MOVIES...."
 python3 bot.py
